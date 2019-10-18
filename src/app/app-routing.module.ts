001d8333258a5f15847d3baa453c2fcefebb8ddd
June Jung <costalgia@gmail.com>
2019-10-18 14:53:11 -0700

    updated structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { InspectionsComponent } from './inspections/inspections.component';
 import { ReportsComponent } from './reports/reports.component';
+import { ReportsViewingComponent } from './reports-viewing/reports-viewing.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'properties', component: PropertiesComponent},
   {path: 'inspections', component: InspectionsComponent},
   {path: 'reports', component: ReportsComponent},
+  {path: 'reports-viewing', component: ReportsViewingComponent},
   {path: 'profile', component: ProfileComponent}
 ];
 
