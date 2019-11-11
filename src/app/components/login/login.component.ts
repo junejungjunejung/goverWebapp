70858517af1f56d0a4663cd50a489067a1b3823c
June Jung <costalgia@gmail.com>
2019-11-10 23:25:09 -0800

    login function ing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { UserService } from '../../services/user.service';
+import { UserService, AuthResponseData } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +10,33 @@ import { UserService } from '../../services/user.service';
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+  
+  onSubmit(form: NgForm) {
 
+    if (!form.valid) {
+      return;
+      // if form is not valid, stop the execution
+    }
+    const email = form.value.email;
+    const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;    
+    authObs = this.userService.login(email, password);
 
-  // form.reset();
+    // authObs.subscribe(
+    //   resData => {
+    //     // console.log(resData);
+    //     this.isLoading = false;
+    //     this.router.navigate(['']);
+    //   },
+    //   errorMessage => {
+    //     console.log(errorMessage);
+    //     this.error = errorMessage;
+    //     this.isLoading = false;
+    //   }
+    // );
+
+    form.reset();
+  }
 }
