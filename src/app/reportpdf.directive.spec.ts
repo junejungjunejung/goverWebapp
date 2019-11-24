10502fc4f502e4499349776cee69132a4c733c7c
DenisCalixto <deniscalixto@gmail.com>
2019-11-23 20:35:40 -0800

    ReportPDF directive

---
+++ src/app/reportpdf.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ReportpdfDirective } from './reportpdf.directive';
+
+describe('ReportpdfDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ReportpdfDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/reportpdf.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appReportpdf]'
+})
+export class ReportpdfDirective {
+
+  @Input('appReportpdf') pdfsource: string;
+
+  constructor(private el: ElementRef) {
+    //el.nativeElement.data = 'https://greenfillproject.com/media/reports/report_21.pdf';
+    //el.nativeElement.data = this.pdfsource;
+     console.log(this.pdfsource);
+  }
+
+  ngOnChanges(changes) {
+    //console.log(this.pdfsource);
+    this.changesrc(this.pdfsource);
+  }
+
+  private changesrc(source: string) {
+    //console.log(this.pdfsource);
+    this.el.nativeElement.data = source;
+  }
+
+}
